3fec2cac3ee6cdcc9e8edb1a7186eb79277e37ca
Toby Bradshaw <tobybrad@microsoft.com>
2017-05-16 16:03:55 -0700

    Fill out the language handling stuff

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 var nconf = require('nconf');
+var request = require('request');
 var restify = require('restify');
 var builder = require('botbuilder');
 var config = nconf.env().argv().file({file: 'localConfig.json'});
+
+function _askLUIS(appId, subKey, q) {
+    var uri = `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${appId}?subscription-key=${subKey}&verbose=true&q=${q}`;
+
+    return new Promise((resolve, reject) => {
+        var options = {
+            uri: uri,
+            method: 'GET'
+        };
+        request(options, (err, response, body) => {
+            resolve(JSON.parse(body));
+        })
+    })
+}
+
+function askLUIS(q) {
+    return _askLUIS(config.get("LUIS_APP_ID"), config.get("LUIS_SUBSCRIPTION_KEY"), q);
+}
+
+function turnOn(session, switchName) {
+}
+
+function turnOff(session, switchName) {
+}
+
+function queryState(session) {
+}
 
 function main() {
 
@@ -25,7 +53,34 @@ function main() {
     //=========================================================
 
     bot.dialog('/', function (session) {
-        session.send("Bork");
+        askLUIS(session.message.text)
+        .then((response) => {
+            switch (response.topScoringIntent.intent) {
+                case "turnOn" : {
+                    if (response.entities.length > 0) {
+                        turnOn(session, response.entities[0].entity);
+                    }
+                    else {
+                        turnOn(session);
+                    }
+                }
+                break;
+
+                case "turnOff" : {
+                    if (response.entities.length > 0) {
+                        turnOff(session, response.entities[0].entity);
+                    }
+                    else {
+                        turnOff(session);
+                    }
+                }
+                break;
+
+                default : {
+                    session.send("Come again?");
+                }
+            }
+        });
     });
 }
 
